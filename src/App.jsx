0250387bec7d3ae031bb2991cfd9925e9cdab606
Sachin Sharma <thapak.krishna2125@gmail.com>
2026-02-07 23:07:38 +0530

    Learn to apply ladder if-else in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-import ReactState from './state.jsx'
-import PracticeState from './practiceState/practice.jsx'
+import { useState } from 'react'
 
+// function Looping() {
+// console.log("da")
+// }
 
 function App() {
+  const [count, setCount] = useState(0);
+
+
+
   return (
     <div>
-      <h1>This is my App.jsx</h1>
-      <ReactState />
-      <PracticeState />
+      <h1>This is my App.jsx {count}</h1>
+      <h1>Let's See how can we apply the if-else ladder in React components</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      {count > 3 && <button onClick={() => setCount(count - 1)}>Decrement</button>}
+      {
+        count == 0 ? <h1>The count is {count}</h1> :
+          count == 1 ? <h1>The count is {count}</h1> :
+            count == 2 ? <h1>The count is {count}</h1> :
+              count == 3 ? <h1>The count is {count}</h1> :
+                <h1>The count went above {count - 1}</h1>
+
+      }{/*Thsi sis how we can use the if-else ladder in React components*/ }
     </div>
   )
 }
