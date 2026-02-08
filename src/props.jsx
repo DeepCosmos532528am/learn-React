4491913bfd8a1719239d867cffd204b4097c44f1
Sachin Sharma <thapak.krishna2125@gmail.com>
2026-02-08 22:41:13 +0530

    feat: learn to pass variable , obj and array to another component from one component using props

--- src/props.jsx
+++ src/props.jsx
@@ -12,20 +12,47 @@ Data Flow: React mein data ek direction mein flow karta hai — Parent to Child.
 import { useState } from "react";
 
 //Here this is the user Component, now we are required to display the name of the user in the user component itself.So we will use the 'props' mechanism here. 
-function User(props) {
-    {/**Or we can also use function User ({MeraName},{age}){} directly.Then we can use MeraName directly to print the came value through props*/}
+function User1(props) {
+    {/**Or we can also use function User ({MeraName},{age}){} directly.Then we can use MeraName directly to print the came value through props*/ }
     console.log(props)
     return (<div>
         <h1>Hello I am from the User Component</h1>
         <h1>From User Component: Hello {props.MeraName}</h1>
         <h1>Age: {props.age}</h1>
-        </div>
+    </div>
     )
 }
 
+// Object passed------------------------------------------
+
+function User2(props){ //this method is called standard method to pass the object through props and then we can access the value of the object through props.passobj.name   
+    return(<h1>{props.passobj.name}</h1>)
+}
+// OR
+
+function User22({passobj = age}){//Is point pe hi automatically through destructuring mechanism, props. ho jata h. This method is called destructuring method to pass the object through props and then we can access the value of the object directly through passobj.name.
+
+    return(<h1>Hello: {passobj.name} and age is {age}</h1>)
+}
+
+//Destructuring is the preffered way to pass the object through props because it is more concise and easier to work with. It allows you to directly access the properties of the object without having to reference the props object each time.
+
+//here in the terms of the object passing know these,
+//  How {} Equals props:
+// When you write { passobj, age } in the parameter, React automatically pulls these properties from the props object and makes them directly available in the component.
+
+// This is equivalent to accessing them through props.passobj or props.age but is more concise and easier to work with. 
+
+//Array Passing through props-----------------------------
+function User3({arr, secValue}){ //Here we are passing the array through props and then we can access the value of the array through arr[0] or secValue.
+    return(
+        <h1>This is the Array: First value {arr[0]}, Second value {secValue}</h1>
+    )
+}
 
 function Main() {
 
+    //For Input Value to show in JSX element on clicking submit button.
     const [name, setname] = useState("")
     const [onPageName, setNameOnPage] = useState("")
 
@@ -33,12 +60,22 @@ function Main() {
         console.log(e.target.value)
         setname(e.target.value)
     }
-
     const finalValueSet = (e) => {
         e.preventDefault()
         setNameOnPage(name)
     }
 
+    //Object to pass to the User2 and User22 component
+
+    const obj = {
+        name: "Rohit",
+        age: 21,
+        city: "Gwl"
+    }
+
+    //Array to pass to the User3 component
+
+    const arr = ["Rohit", "Amit", "Rahul", "Suresh"]
 
     return (
         <div>
@@ -47,7 +84,15 @@ function Main() {
                 <input type="text" onChange={setValue} />
                 <input type="submit" />
             </form>
-            <User MeraName={onPageName} age={21} /> {/**Here the MeraName is the props  */}
+            {/**Here the MeraName is the variable props passed to User Component*/}
+            <User1 MeraName={onPageName} age={21} /> 
+
+            {/**Here object is passed*/}
+            <User2 passobj = {obj}/>
+            <User22 passobj = {obj} age={obj.age}/>
+
+            {/**Here array is passed*/}
+            <User3 arr = {arr} secValue={arr[1]}/>
 
         </div>
     )
