e32bfd19146f35a5b0f250c40712e8a9d5719881
Sachin Sharma <thapak.krishna2125@gmail.com>
2026-02-15 22:14:57 +0530

    feat: learn to play with objStates i.e. update and accessing the object state

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import ReactState from './state.jsx'
 import PracticeState from './practiceState/practice.jsx'
+import ObjectState from './objectState.jsx';
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <h1>This is my App.jsx</h1>
       <ReactState />
       <PracticeState />
+      <ObjectState />
     </div>
   )
 }
