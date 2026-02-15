21aae375b0d3afc68496e56b63a33aeff7015a8c
Sachin Sharma <thapak.krishna2125@gmail.com>
2026-02-16 01:19:08 +0530

    add: implement JoinTeam form using React 19 useActionState for native async state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import ReactState from './state.jsx'
-import PracticeState from './practiceState/practice.jsx'
-import ObjectState from './objectState.jsx';
-import ArrayStateMaster from './arrayState.jsx';
+import JoinTeamForm from "./useActionStateHook";
 
 
 function App() {
   return (
     <div>
       <h1>This is my App.jsx</h1>
-      <ReactState />
-      <PracticeState />
-      <ObjectState />
-      <ArrayStateMaster/>
+      <JoinTeamForm />
+      
     </div>
   )
 }
